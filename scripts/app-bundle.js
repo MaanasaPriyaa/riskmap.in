a4fb3ea342b347f27c9e2973e2d310714d2a2d0f
MaanasaPriyaa <maanasa@mit.edu>
2017-03-13 13:28:38 -0400

    Build bundle

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -19,7 +19,7 @@ define('app',['exports'], function (exports) {
     App.prototype.configureRouter = function configureRouter(config, router) {
       config.title = 'Riskmap.in';
       config.options.pushState = true;
-      config.options.root = '/';
+      config.options.root = '/riskmap.in/';
       config.map([{ route: ['', 'map', 'map/:city', 'map/:city/'], name: 'map', moduleId: 'routes/landing/landing' }, { route: 'map/:city/:report', moduleId: 'routes/landing/landing' }, { route: 'cards/:disaster/:id', moduleId: 'routes/cards/cards' }]);
       config.mapUnknownRoutes({ moduleId: 'routes/landing/landing' });
       this.router = router;
